ee1390d7d506cb58a39eb8ffef2b317053ebc762
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-11-20 15:33:40 +0700

    Define winner

--- src/index.js
+++ src/index.js
@@ -86,6 +86,7 @@ class Game extends React.Component {
       ],
       stepNumber: 0,
       historyClicked: [],
+      winnerMark: Array(ROW*COL).fill(null),
       xIsNext: true
     };
   }
@@ -96,7 +97,7 @@ class Game extends React.Component {
     const squares = current.squares.slice();
     try{
       console.log(this.state.historyClicked[this.state.historyClicked.length-1] + '==' + this.state.xIsNext);
-      if (calculateWinner(squares, this.state.historyClicked[this.state.historyClicked.length-1], !this.state.xIsNext) || squares[i]) {
+      if (this.calculateWinner(squares, this.state.historyClicked[this.state.historyClicked.length-1], !this.state.xIsNext) || squares[i]) {
         return;
       }
     }
@@ -144,10 +145,153 @@ class Game extends React.Component {
     });
   }
 
+  
+  calculateWinner(squares, clickedSquare, isX) 
+  {
+
+    if(clickedSquare === -1){
+      return null;
+    }
+
+    const checkValue = isX ? 'X' : 'O';
+
+    var numberOfSameSquares = 0;
+    var currRow = numToRow(clickedSquare);
+    var currCol = numToCol(clickedSquare);
+    var winnerMarkTemp;
+
+    //check row
+    for(var col = 0; col < COL; col++)
+    {
+      winnerMarkTemp = Array(COL*ROW).fill(null);
+      numberOfSameSquares = 0;
+      for(var i = col; i < COL; i++){
+
+        if(squares[cordToNum(currRow,i)] === checkValue){
+          numberOfSameSquares++;
+          winnerMarkTemp[cordToNum(currRow,i)] = true;
+        }
+        else{
+          break;
+        }
+      }
+      if(numberOfSameSquares === WIN_CONDITION){
+        // this.setState({
+        //   winnerMark: winnerMarkTemp
+        // })
+        return checkValue;
+      }
+    }
+
+    //check column
+    for(var row = 0; row < ROW; row++)
+    {
+      winnerMarkTemp = Array(COL*ROW).fill(null);
+      numberOfSameSquares = 0;
+      for(let i = row; i < ROW; i++){
+
+        if(squares[cordToNum(i,currCol)] === checkValue){
+          numberOfSameSquares++;
+          winnerMarkTemp[cordToNum(i,currCol)] = true;
+        }
+        else{
+          break;
+        }
+      }
+      if(numberOfSameSquares === WIN_CONDITION){
+        // this.setState({
+        //   winnerMark: winnerMarkTemp
+        // })
+        return checkValue;
+      }
+  }
+
+  //check left-right-cross
+  var leftRightCrossInfo = leftRightCrossCord(clickedSquare);
+  currCol = leftRightCrossInfo[0][1];
+  for(let row = leftRightCrossInfo[0][0]; row < leftRightCrossInfo[1][0]; row++)
+  {
+    winnerMarkTemp = Array(COL*ROW).fill(null);
+    numberOfSameSquares = 0;
+    let currColTemp = currCol;
+    for(let i = row; i < leftRightCrossInfo[1][0]; i++){
+
+      if(squares[cordToNum(i,currColTemp)] === checkValue){
+        numberOfSameSquares++;
+        currColTemp++;
+        winnerMarkTemp[cordToNum(i,currColTemp)] = true;
+      }
+      else{
+        break;
+      }
+    }
+    currCol++;
+    if(numberOfSameSquares === WIN_CONDITION){
+      // this.setState({
+      //   winnerMark: winnerMarkTemp
+      // })
+      return checkValue;
+    }
+  }
+
+   //check right-left-cross
+   var rightLeftCrossInfo = rightLeftCrossCord(clickedSquare);
+   currCol = rightLeftCrossInfo[0][1];
+   for(let row = rightLeftCrossInfo[0][0]; row < rightLeftCrossInfo[1][0]; row++)
+   {
+     winnerMarkTemp = Array(COL*ROW).fill(null);
+     numberOfSameSquares = 0;
+     let currColTemp = currCol;
+     for(let i = row; i <= rightLeftCrossInfo[1][0]; i++){
+ 
+       if(squares[cordToNum(i,currColTemp)] === checkValue){
+         numberOfSameSquares++;
+         currColTemp--;
+         winnerMarkTemp[cordToNum(i,currColTemp)] = true;
+       }
+       else{
+         break;
+       }
+     }
+     currCol--;
+     if(numberOfSameSquares === WIN_CONDITION){
+       // this.setState({
+       //   winnerMark: winnerMarkTemp
+       // })
+       return checkValue;
+     }
+   }
+  // //check every single line and crossline
+  // //check rows
+  // console.log(squares);
+  // for(var row=0; row < ROW; row++){
+  //   for(var col=0; col <COL; col++){
+  //     var checkValue = squares[cordToNum(row, col)];
+  //     var numberOfSameSquares = 0;
+  //     for(var i = col; i<COL; i++)
+  //     {
+        
+  //       if(checkValue === null)
+  //       {
+  //         break;
+  //       }
+  //       if(squares[cordToNum(row, i)] === checkValue){
+  //         numberOfSameSquares++;
+  //       }
+  //       if(numberOfSameSquares === WIN_CONDITION)
+  //       {
+  //         return checkValue;
+  //       }
+  //     }
+  //   }
+  // }
+  return null;
+}
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares, this.state.historyClicked[this.state.historyClicked.length-1], !this.state.xIsNext);
+    const winner = this.calculateWinner(current.squares, this.state.historyClicked[this.state.historyClicked.length-1], !this.state.xIsNext);
 
     const moves = history.map((step, move) => {
       var rowAndCol = '';
@@ -200,62 +344,6 @@ class Game extends React.Component {
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 
-function calculateWinner(squares, clickedSquare, isX) {
-  if(clickedSquare === -1){
-    return null;
-  }
-
-  const checkValue = isX ? 'X' : 'O';
-
-  //check row
-  var numberOfSameSquares = 0;
-  const row = numToRow(clickedSquare);
-
-  for(var col = 0; col < COL; col++)
-  {
-    numberOfSameSquares = 0;
-    for(var i = col; i < COL; i++){
-
-      if(squares[cordToNum(row,i)] === checkValue){
-        numberOfSameSquares++;
-      }
-      else{
-        break;
-      }
-    }
-    if(numberOfSameSquares === WIN_CONDITION){
-      return checkValue;
-    }
-  }
-
-
-  
-  // //check every single line and crossline
-  // //check rows
-  // console.log(squares);
-  // for(var row=0; row < ROW; row++){
-  //   for(var col=0; col <COL; col++){
-  //     var checkValue = squares[cordToNum(row, col)];
-  //     var numberOfSameSquares = 0;
-  //     for(var i = col; i<COL; i++)
-  //     {
-        
-  //       if(checkValue === null)
-  //       {
-  //         break;
-  //       }
-  //       if(squares[cordToNum(row, i)] === checkValue){
-  //         numberOfSameSquares++;
-  //       }
-  //       if(numberOfSameSquares === WIN_CONDITION)
-  //       {
-  //         return checkValue;
-  //       }
-  //     }
-  //   }
-  // }
-  return null;
-}
 
 function numToCol(num){
   return (num - 1) % COL;
@@ -269,4 +357,58 @@ function numToRow(num){
 function cordToNum(row, col)
 {
   return ROW*row + col + 1;
+}
+
+//array[begin||end point][row||col]
+function leftRightCrossCord(num){
+  var row = numToRow(num);
+  var col = numToCol(num);
+
+  var result = Array(2).fill(null);
+
+  while(col>0 || row >0)
+  {
+    col--;
+    row--;
+  }
+  result[0] = [row, col];
+
+  row = numToRow(num);
+  col = numToCol(num);
+
+  while(col<COL || row <ROW)
+  {
+    col++;
+    row++;
+  }
+  result[1] = [row, col];
+
+  return result;
+}
+
+//array[begin||end point][row||col]
+function rightLeftCrossCord(num){
+  var row = numToRow(num);
+  var col = numToCol(num);
+
+  var result = Array(2).fill(null);
+
+  while(col<COL && row >0)
+  {
+    col++;
+    row--;
+  }
+  result[0] = [row, col];
+
+  row = numToRow(num);
+  col = numToCol(num);
+
+  while(col>0 && row <ROW)
+  {
+    col--;
+    row++;
+  }
+  result[1] = [row, col];
+
+  return result;
 }
